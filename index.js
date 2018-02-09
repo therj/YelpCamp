c61d78e947c4ef1b3ab7cb636547e829bda1a0be
Rabindra Joshi <therj@users.noreply.github.com>
2018-02-10 00:22:15 +0545

    form & bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 //Temporary --> MongoDB
 var campgrounds = [
     {
@@ -17,13 +20,30 @@ var campgrounds = [
     }
 ];
 
-app.set("view engine", "ejs");
+
+
 app.get("/", function (req, res) {
     res.render("landing");
 });
+
 app.get("/campgrounds", function (req, res) {
     res.render("campgrounds", {campgrounds: campgrounds});
 });
+
+app.post("/campgrounds", function (req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function (req, res) {
+    //show form
+    res.render("new");
+});
+
+
 
 
 
